4e6c3dc482609bf57d14faf1bac4a609c3282bd0
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2019-08-30 13:10:08 -0500

    Updated building

--- src/main/java/net/manmaed/cookies/Cookies.java
+++ src/main/java/net/manmaed/cookies/Cookies.java
@@ -14,7 +14,6 @@ public class Cookies implements ModInitializer {
     public static final String MOD_ID = "cookies";
     public final static UnRealRegistry REGISTRY = new UnRealRegistry(MOD_ID);
     public final static Logger LOGGER = LogManager.getLogger();
-//    public final static CustomTreeGen worldGenTree = WorldGenUtil.createBaiscTree();
 
     @Override
     public void onInitialize() {
@@ -25,8 +24,6 @@ public class Cookies implements ModInitializer {
         REGISTRY.registerEnumsAsItems(CookieItems.Basic.class);
 
         REGISTRY.registerEnumAsContainer(CookieContainers.class);
-//        Registry.register(Registry.FEATURE, new Identifier(MOD_ID, "tree"), worldGenTree);
-//        Registry.BIOME.stream().forEach(biome -> biome.addFeature(GenerationStep.Feature.VEGETAL_DECORATION, configureFeature(Feature.RANDOM_SELECTOR, new RandomFeatureConfig(new Feature[]{worldGenTree}, new FeatureConfig[]{FeatureConfig.DEFAULT}, new float[]{0.025F}, Feature.NORMAL_TREE, FeatureConfig.DEFAULT), Decorator.DARK_OAK_TREE, DecoratorConfig.DEFAULT)));
 
     }
 }
